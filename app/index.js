30346b353e2ce2fa784f96b97cfed61c31a25908
熊杰 <xiongj01@mySoft.com.cn>
2016-12-17 16:31:39 +0800

    init

--- app/index.js
+++ app/index.js
@@ -1,6 +1,16 @@
 import koa from "koa";
 const app = new koa()
-console.log(app)
-const a = async (ctx,next) => {
-    await next();
+function sleep(x) {
+    return new Promise((resolve) => {
+        setTimout(()=>{
+            resolve(x)
+        },300)
+    })
 }
+const a = async function () {
+    for (let i = 0;i < 10; i++) {
+        let x = await sleep(i)
+        console.log(x)
+    }
+}
+a();
