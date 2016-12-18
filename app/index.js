95e8107474cf9a811fe7f8c953f796da61a27908
cexoso <285516193@qq.com>
2016-12-18 12:10:23 +0800

    frouter不好用

--- app/index.js
+++ app/index.js
@@ -1,16 +1,9 @@
 import koa from "koa";
-const app = new koa()
-function sleep(x) {
-    return new Promise((resolve) => {
-        setTimout(()=>{
-            resolve(x)
-        },300)
-    })
-}
-const a = async function () {
-    for (let i = 0;i < 10; i++) {
-        let x = await sleep(i)
-        console.log(x)
-    }
-}
-a();
+import router from "koa-frouter";
+import path from "path";
+const app = new koa();
+app.use(router(app,{
+    root: path.join(__dirname,"./router"),
+    wildcard: "_"
+}));
+app.listen(3000);
---
+++ app/router/use/_id.js
@@ -0,0 +1,6 @@
+function* get(uid, next) {
+  this.body = "xxx"
+};
+
+export default {get};
+exports.get = get;
